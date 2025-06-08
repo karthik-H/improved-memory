580a1e86b0e4261bd422212fc29e7cdc67bc4398
karthik-H <karthikh@iastate.edu>
2025-06-08 00:54:08 -0500

    initial commit

---
+++ test_program.py
@@ -0,0 +1,16 @@
+import re
+
+
+def validate_user_email(email: str) -> bool:
+    valid_domain = ["gmail.com", "yahoo.com", "outlook.com"]
+    # Simple regex for validating an email address
+    pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+    if re.match(pattern, email) is None:
+        return "Invalid email format"
+
+    domain = email.split("@")[-1]
+
+    if domain not in valid_domain:
+        return "Invalid email domain"
+    else:
+        return "Valid email address"
