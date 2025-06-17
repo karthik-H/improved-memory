29c423cfb9ef939fde8eb1546010e94012d738d8
karthik-H <karthikh@iastate.edu>
2025-06-17 00:30:36 -0500

    Update test_program.py

--- test_program.py
+++ test_program.py
@@ -4,7 +4,7 @@ import re
 def validate_user_email(email: str) -> bool:
     valid_domain = ["gmail.com", "yahoo.com"]
     # Simple regex for validating an email address
-    pattern = r"^[a-zA-Z0-9._%+-]+@@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+    pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
     if re.match(pattern, email) is None:
         return "Invalid email format"
 
